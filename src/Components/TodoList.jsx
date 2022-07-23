a29efea512125c56c9db446e27bdbefa8e4fd730
Javon Townsend <townsend.javon@gmail.com>
2022-07-22 21:13:51 -0400

    Fix clipboard text bar width

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -6,20 +6,20 @@ const TodoList = ({ todos, handleDelete, handleEdit }) => {
       {todos.map((t) => (
         <li className="flex m-2 w-[55%] justify-between">
           <span
-            className="w-full mr-1 p-2 overflow-hidden rounded-lg bg-slate-500"
+            className="w-screen mr-1 p-2 overflow-hidden rounded-lg bg-slate-500"
             key={t.id}
           >
             {t.todo}
           </span>
           <div className="flex">
             <button
-              className=" mx-1 text-white bg-cyan-500 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+              className=" mx-1 text-white bg-cyan-500 hover:bg-cyan-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
               onClick={() => handleEdit(t.id)}
             >
               Edit
             </button>
             <button
-              className=" mx-1 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+              className=" mx-1 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
               onClick={() => handleDelete(t.id)}
             >
               Delete
